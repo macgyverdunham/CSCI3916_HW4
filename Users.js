8e64b0ffc3da010ff41c9b7e80ae9a1d5350a585
MacGyver Dunham <macgyver.dunham@gmail.com>
2021-03-09 15:30:31 -0700

    hw3 - second commit (updated Users.js per Announcement)

--- Users.js
+++ Users.js
@@ -4,13 +4,19 @@ var bcrypt = require('bcrypt-nodejs');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.DB, { useNewUrlParser: true});
+//mongoose.connect(process.env.DB, { useNewUrlParser: true });
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
 mongoose.set('useCreateIndex', true);
 
 //user schema
 var UserSchema = new Schema({
     name: String,
-    username: { type: String, required: true, index: { unique: true}},
+    username: { type: String, required: true, index: { unique: true }},
     password: { type: String, required: true, select: false }
 });
 
@@ -32,7 +38,7 @@ UserSchema.pre('save', function(next) {
 UserSchema.methods.comparePassword = function (password, callback) {
     var user = this;
 
-    bcrypt.compare(password, user.password, function(err, isMatch){
+    bcrypt.compare(password, user.password, function(err, isMatch) {
         callback(isMatch);
     })
 }
